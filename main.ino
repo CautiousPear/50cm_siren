589a0ef5bc3fc42fb7ca48d5740fdeb6f682b9b3
CautiousPear <gtnevsimal@gmail.com>
2025-06-09 16:57:09 -0500

    Added passive buzzer support

--- main.ino
+++ main.ino
@@ -3,21 +3,21 @@ const int echoPin = 10;
 const int sirenPin = 11;
 
 float distance;
-float duration; //Duration until wave return
+float duration;
 
-#include <LiquidCrystal.h> //LCD library
+#include <LiquidCrystal.h>
 
 const int rs = 7, en = 6, d4 = 2, d5 = 3, d6 = 4, d7 = 5;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(sirenPin, OUTPUT);
   digitalWrite(sirenPin, LOW);
-  Serial.begin(9600); //9600 baud monitor
-  lcd.begin(16, 2); //display size
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  
 }
 
 void loop() {
@@ -29,34 +29,32 @@ void loop() {
   
   duration = pulseIn(echoPin, HIGH, 30000);  // 30ms timer
   
-  if (duration == 0) { // If no sound back in 30ms, read invalid
+  if (duration == 0) { // If no sound is detected in 30ms
     Serial.println("No Echo Detected");
     lcd.setCursor(0, 0);
     lcd.print("No Echo Detected");
   } else {
     distance = duration * 0.0343 / 2; 
-    
+
     Serial.print("Distance: ");
     Serial.print(distance);
     Serial.println(" cm");
 
     lcd.clear();  // Clear LCD before printing
-    lcd.setCursor(0, 0); //Start LCD print at top leftmost square
+    lcd.setCursor(0, 0);
     lcd.print("Dist: ");
     lcd.print(distance);
     lcd.print(" cm");
-
-    lcd.setCursor(0, 1); //Start LCD at bottom leftmost square
-    lcd.print("Uptime: ");
-    lcd.print(millis() / 1000);
-    lcd.print("s  ");
-    
-  if (distance > 0 && distance < 50) { //Siren if >0cm and <50cm
-    digitalWrite(sirenPin, HIGH);
-    Serial.println("Siren ON");
+  }
+  if ((distance <= 50) && (distance != 0)) {
+      tone(sirenPin, (2000-(40*distance))); //Siren frequency
   } else {
-    digitalWrite(sirenPin, LOW); 
+      noTone(sirenPin); //Stop if >50cm or none detected
   }
-  delay(500);
-  }
+  lcd.setCursor(0, 1);
+  lcd.print("Uptime: ");
+  lcd.print(millis() / 1000);
+  lcd.print("s  ");
+
+  delay(100);
 }
